3d671a6242ac9660dcc829684884d4f17e31144f
QiaoLiu <smileqiao1988@yahoo.com>
2020-12-06 15:58:13 -0500

    update pretraining

--- script/train_deepce_ehill_pretraining.sh
+++ script/train_deepce_ehill_pretraining.sh
@@ -4,7 +4,7 @@ CUDA_VISIBLE_DEVICES=0 python ../DeepCE/pretrain_ehill_deepce.py --drug_file "..
 --gene_file "../DeepCE/data/gene_vector.csv"  --hill_train_file "../DeepCE/data/signature_train_pretraining_ehill_unit.csv" \
 --hill_dev_file "../DeepCE/data/signature_dev_pretraining_ehill_unit.csv" \
 --hill_test_file "../DeepCE/data/signature_test_pretraining_ehill_unit.csv" \
---train_fiile "../DeepCE/data/signature_train_cell_3.csv" \
+--train_file "../DeepCE/data/signature_train_cell_3.csv" \
 --dev_file "../DeepCE/data/signature_dev_cell_3.csv" \
 --test_file "../DeepCE/data/signature_test_cell_3.csv" \
 --dropout 0.1 --batch_size 64 --max_epoch 100 --unfreeze_steps 0,0,0,0 \
