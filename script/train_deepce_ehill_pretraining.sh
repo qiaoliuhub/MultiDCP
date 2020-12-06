fc322da903a5fc312c5ab7216303940b4ef344d3
QiaoLiu <smileqiao1988@yahoo.com>
2020-12-06 11:46:41 -0500

    add unfreeze

--- script/train_deepce_ehill_pretraining.sh
+++ script/train_deepce_ehill_pretraining.sh
@@ -3,4 +3,5 @@
 CUDA_VISIBLE_DEVICES=3 python ../DeepCE/pretrain_ehill_deepce.py --drug_file "../DeepCE/data/drug_smiles_new.csv" \
 --gene_file "../DeepCE/data/gene_vector.csv"  --train_file "../DeepCE/data/signature_train_pretraining_ehill_unit.csv" \
 --dev_file "../DeepCE/data/signature_dev_pretraining_ehill_unit.csv" --test_file "../DeepCE/data/signature_test_pretraining_ehill_unit.csv" \
---dropout 0.1 --batch_size 64 --max_epoch 100 --all_cells "../DeepCE/data/pretrain_cell_list_ehill.p" --cell_ge_file "../DeepCE/data/gene_expression_combat_norm_978" # > ../DeepCE/output/cellwise_output_ran5.txt
+--dropout 0.1 --batch_size 64 --max_epoch 100 --unfreeze_steps 0,0,0,0 --all_cells "../DeepCE/data/pretrain_cell_list_ehill.p" \
+--cell_ge_file "../DeepCE/data/gene_expression_combat_norm_978" # > ../DeepCE/output/cellwise_output_ran5.txt
