719d33fae67effa9b0b2891c525af6648d74a719
qiao liu <qiaoliu@qiaos-mac-mini.lan>
2020-12-13 00:29:33 -0500

    linear finetune split2

--- script/train_deepce_ehill_pretraining.sh
+++ script/train_deepce_ehill_pretraining.sh
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=1 python ../DeepCE/pretrain_ehill_deepce.py --drug_file "..
 --train_file "../DeepCE/data/signature_train_cell_3.csv" \
 --dev_file "../DeepCE/data/signature_dev_cell_3.csv" \
 --test_file "../DeepCE/data/signature_test_cell_3.csv" \
---dropout 0.3 --batch_size 64 --max_epoch 100 --unfreeze_steps 0,0,0,0 \
+--dropout 0.3 --batch_size 256 --max_epoch 100 --unfreeze_steps 0,0,0,0 \
 --all_cells "../DeepCE/data/pretrain_cell_list_ehill.p" \
 --cell_ge_file "../DeepCE/data/gene_expression_combat_norm_978" \
 --linear_only #> ../DeepCE/output/cellwise_output_ran5.txt
