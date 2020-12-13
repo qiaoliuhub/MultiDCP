a4225e8444c4541124e6737507a2c1282f7af7a5
qiao liu <qiaoliu@qiaos-mac-mini.lan>
2020-12-13 00:17:28 -0500

    pertrain linear

--- script/train_deepce_ehill_pretraining.sh
+++ script/train_deepce_ehill_pretraining.sh
@@ -10,4 +10,4 @@ CUDA_VISIBLE_DEVICES=1 python ../DeepCE/pretrain_ehill_deepce.py --drug_file "..
 --dropout 0.3 --batch_size 64 --max_epoch 100 --unfreeze_steps 0,0,0,0 \
 --all_cells "../DeepCE/data/pretrain_cell_list_ehill.p" \
 --cell_ge_file "../DeepCE/data/gene_expression_combat_norm_978" \
- #> ../DeepCE/output/cellwise_output_ran5.txt
+--linear_only #> ../DeepCE/output/cellwise_output_ran5.txt
