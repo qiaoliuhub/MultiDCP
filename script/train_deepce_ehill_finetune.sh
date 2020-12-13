2830e7d2d59b1a52c733cf84383ac1a249ee3a3a
qiao liu <qiaoliu@qiaos-mac-mini.lan>
2020-12-13 14:53:34 -0500

    small input

--- script/train_deepce_ehill_finetune.sh
+++ script/train_deepce_ehill_finetune.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 CUDA_VISIBLE_DEVICES=7 python ../DeepCE/finetune_ehill_deepce.py --drug_file "../DeepCE/data/drug_smiles_new.csv" \
---gene_file "../DeepCE/data/gene_vector.csv"  --hill_train_file "../DeepCE/data/signature_train_pretraining_ehill_unit.csv" \
---hill_dev_file "../DeepCE/data/signature_dev_pretraining_ehill_unit.csv" \
---hill_test_file "../DeepCE/data/signature_test_pretraining_ehill_unit.csv" \
+--gene_file "../DeepCE/data/gene_vector.csv"  --hill_train_file "../DeepCE/data/signature_train_pretraining_ehill_unit_tri.csv" \
+--hill_dev_file "../DeepCE/data/signature_dev_pretraining_ehill_unit_tri.csv" \
+--hill_test_file "../DeepCE/data/signature_test_pretraining_ehill_unit_tri.csv" \
 --train_file "../DeepCE/data/signature_train_cell_2.csv" \
 --dev_file "../DeepCE/data/signature_dev_cell_2.csv" \
 --test_file "../DeepCE/data/signature_test_cell_2.csv" \
