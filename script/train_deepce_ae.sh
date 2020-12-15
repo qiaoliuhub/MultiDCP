da7186867ff9f7a903c8e5424c6d7a24d359e2b9
qiao liu <qiaoliu@qiaos-mac-mini.lan>
2020-12-15 10:31:47 -0500

    pretrain with two models

--- script/train_deepce_ae.sh
+++ script/train_deepce_ae.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=1 python ../DeepCE/deepce_ae.py --drug_file "../DeepCE/data/drug_smiles_new.csv" \
+CUDA_VISIBLE_DEVICES=5 python ../DeepCE/deepce_ae.py --drug_file "../DeepCE/data/drug_smiles_new.csv" \
 --gene_file "../DeepCE/data/gene_vector.csv"  --train_file "../DeepCE/data/signature_train_cell_3.csv" \
 --dev_file "../DeepCE/data/signature_dev_cell_3.csv" --test_file "../DeepCE/data/signature_test_cell_3.csv" \
 --dropout 0.3 --batch_size 64 --max_epoch 800 --unfreeze_steps 0,0,0,0 \
