3fcf44ae97389ebf9630a4232edd0c620a0a4785
root <root@dls1.cluster.local>
2021-02-17 18:39:04 +0000

    dave data

--- script/train_deepce_ehill_mt.sh
+++ script/train_deepce_ehill_mt.sh
@@ -8,6 +8,6 @@ CUDA_VISIBLE_DEVICES=7 python ../DeepCE/ehill_deepce_mt.py --drug_file "../DeepC
 --dev_file "../DeepCE/data/signature_dev_cell_2.csv" \
 --test_file "../DeepCE/data/signature_test_cell_2.csv" \
 --dropout 0.1 --batch_size 64 --max_epoch 100 --unfreeze_steps 0,0,0,0 \
---all_cells "../DeepCE/data/pretrain_cell_list_ehill.p" \
+--all_cells "../DeepCE/data/pretrain_cell_list_ehill.p" 
 --cell_ge_file "../DeepCE/data/gene_expression_combat_norm_978" \
 --linear_only # > ../DeepCE/output/cellwise_output_ran5.txt
