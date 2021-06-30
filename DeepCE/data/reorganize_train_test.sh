1c9884552e2506fc6331efc2284db535581c7ae3
root <root@dls1.cluster.local>
2021-06-30 19:06:27 +0000

    change name

--- DeepCE/data/reorganize_train_test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-python reorganize_train_test.py --train_data signature_train_cell_3.csv --test_data signature_test_cell_3.csv \
---dev_data signature_dev_cell_3.csv --split_variable cell_id --new_train_data temp_signature_train_cell_3.csv \
---new_test_data temp_signature_test_cell_3.csv --new_dev_data temp_signature_dev_cell_3.csv
--- script/train_deepce_pretraining.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-CUDA_VISIBLE_DEVICES=4 python ../DeepCE/pretrain_deepce.py --drug_file "../DeepCE/data/drug_smiles.csv" \
---gene_file "../DeepCE/data/gene_vector.csv"  --train_file "../DeepCE/data/signature_train_pretraining.csv" \
---dev_file "../DeepCE/data/signature_dev_pretraining.csv" --test_file "../DeepCE/data/signature_test_pretraining.csv" \
---dropout 0.1 --batch_size 64 --max_epoch 500 --all_cells "../DeepCE/data/pretrain_cell_list_ehill.p" --cell_ge_file "../DeepCE/data/gene_expression_combat_norm_978" # > ../DeepCE/output/cellwise_output_ran5.txt
